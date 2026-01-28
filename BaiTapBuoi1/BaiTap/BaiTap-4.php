cf03a6b9828e30e51ea59b362a1210c21e89e2fe
Pham Cong Minh <pham.cong.minh@gmail.com>
2026-01-28 21:55:05 +0700

    cap nhat code buôi 2 cho anh em

--- BaiTapBuoi1/BaiTap/BaiTap-4.php
+++ BaiTapBuoi1/BaiTap/BaiTap-4.php
@@ -4,10 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>trang-4.php</title>
+    <link rel="stylesheet" href="../Css/Baitap4.css">
+    <link rel="icon" href="../Images/kiniem.jpg">
 
 </head>
 <body>
-    <?php
+
+   <div class="khungtong">
+        <!-- phần đầu của trang -->
+        <div class="khunganh">
+            <img src="../images/mm_travel_photo.jpg" alt="Cây dừa">
+
+            <div class="khungchu">
+                <p class="noidung">
+                    CÂU LẠC BỘ TIN HỌC - NGOẠI NGỮ - BDVH
+                </p>
+                <P class="Thongtin">
+                    http://khonggiankythuat.com
+                </P>
+            </div>
+        </div>
+
+        <div class="khungMenu">
+            <ul class="Menu">
+                <li class="_1Menu">Trang chủ</li>
+                <li class="_1Menu">Danh sách sinh viên </li>
+                <li class="_2Menu">Thêm sinh viên</li>
+                <li class="_2Menu">Xóa sinh viên</li>
+            </ul>
+        </div>
+
+        <!-- phần thân của trang -->
+
+        <div class="bang">
+            <table class="table1">
+                <tr>
+                   <th>THÔNG BÁO KẾT QUẢ THI ĐỊA HỌC</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Nguyễn Văn A</td>
+                    
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Trần Thị B</td>
+                    
+                </tr>
+                <tr>
+                    <th>đ</th>
+                </tr>
+            </table>
+        </div>
+
+    <!-- phần kêt của trang -->
+        <div class="kettrang">
+            <b class="indam">
+                CÂU LẠC BỘ TIN HỌC - NGOẠI NGỮ - BỒI DƯỠNG VĂN HÓA
+            </b>
+
+            <P class="noidung1">Địa chỉ 11 Đường 14B, Phường Bình Hưng Hòa A, Quận Bình Tân,Tp.HCM</P>
+            <P class="noidung1">Điện thoại: 0903.97.9218</P>
+            <P class="noidung1">Website: http://khonggiankythuat.com</P>
+        </div>
+    </div>
+    <?php   
+    
             echo "<table 
                     border='3'
                     style='border-collapse: collapse; margin:auto; margin-top:100px;'
---
+++ BaiTapBuoi2/BaiTap/BaiTap-1.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài cây dừa</title>
+    <link rel="stylesheet" href="../Css/BaiTap-1.css">
+
+    <!-- CODE PHP -->
+    <?php
+        if(isset ($_POST['Dai']) && $_POST['Dai']!=null &&
+            isset ($_POST['Rong']) && $_POST['Rong']!=null)
+        {
+          $dientich= $_POST['Dai'] * $_POST['Rong'];
+        }
+
+        else $dientich= "Vui lòng nhập số!";
+     ?>
+
+</head>
+<body>
+    <div class="khungtong">
+        <!-- phần đầu của trang -->
+        <div class="khunganh">
+            <img src="../images/mm_travel_photo.jpg" alt="Cây dừa">
+
+            <div class="khungchu">
+                <p class="noidung">
+                    CÂU LẠC BỘ TIN HỌC - NGOẠI NGỮ - BDVH
+                </p>
+                <P class="Thongtin">
+                    http://khonggiankythuat.com
+                </P>
+            </div>
+        </div>
+
+        <div class="khungMenu">
+            <ul class="Menu">
+                <li class="_1Menu">Trang chủ</li>
+                <li class="_1Menu">Danh sách sinh viên </li>
+                <li class="_1Menu">Thêm sinh viên</li>
+                <li class="_1Menu">Xóa sinh viên</li>
+            </ul>
+        </div>
+
+        <!-- phần thân của trang -->
+<form action="#" method="post">
+
+        <div class="bang">
+                <table class="table1">
+                    
+                    <tr>
+                        <th class="title" colspan="2">
+                            TÍNH DIỆN TÍCH HÌNH CHỮ NHẬT
+                        </th>
+                    </tr>
+
+                    <tr class="box-item">
+                        <td>Chiều dài</td>
+                        <td class="box-enter">
+                            <input type="text" name="Dai"> 
+                        </td>
+                    </tr>
+
+                    <tr class="box-item">
+                        <td>Chiều rộng</td>
+                        <td class="box-enter">
+                            <input type="text" name="Rong">
+                            <button type="submit">                    
+                                Thực hiện
+                            </button>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                       <th class="title" colspan="2">
+                            Kết quả
+                       </th>
+                    </tr>
+
+                    <tr class="box-item">
+                        <td>Diện tích hình chữ nhật</td>
+                        <td class="box-enter">
+                            <input type="text" name="Dientich" disabled 
+                                value="<?php echo $dientich; ?>">
+                        </td>
+                    </tr>
+                </table>
+        </div>
+</form>
+    <!-- phần kêt của trang -->
+        <div class="kettrang">
+            <b class="indam">
+                CÂU LẠC BỘ TIN HỌC - NGOẠI NGỮ - BỒI DƯỠNG VĂN HÓA
+            </b>
+
+            <P class="noidung1">Địa chỉ 11 Đường 14B, Phường Bình Hưng Hòa A, Quận Bình Tân,Tp.HCM</P>
+            <P class="noidung1">Điện thoại: 0903.97.9218</P>
+            <P class="noidung1">Website: http://khonggiankythuat.com</P>
+        </div>
+    </div>
+</body>
+</html>
